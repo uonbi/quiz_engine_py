72e9f6a05febfd78f5e303ef684de5299a8b2b40
Ian Kosen <ikosenn@gmail.com>
2015-03-02 17:44:14 +0300

    Registered the models with the admin platform

--- quiz_engine/quiz_app/admin.py
+++ quiz_engine/quiz_app/admin.py
@@ -6,7 +6,12 @@ from .models import QuizModel
 from .models import MemberModel
 from .models import SubmissionModel
 from .models import SentMessagesModel
-from .models import ReceivedMessages
+from .models import ReceivedMessagesModel
 
 # Register your models here.
 
+admin.site.register(QuizModel)
+admin.site.register(MemberModel)
+admin.site.register(SubmissionModel)
+admin.site.register(SentMessagesModel)
+admin.site.register(ReceivedMessagesModel)
